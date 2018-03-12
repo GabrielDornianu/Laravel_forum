e7f0ae725d2ad7414784bf1819cf4d11bcf38e34
gabrieldornianu <gabrieldornianu@yahoo.com>
2018-03-12 17:38:32 +0200

    Created channels crud, shared channels data among views

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,11 @@ Route::get('/{provider}/redirect', [
   'uses' => 'SocialsController@auth_callback',
   'as'   => 'social.callback'
 ]);
+
+Route::group(['middleware' => 'auth'], function(){
+  Route::resource('channels', 'ChannelsController');
+});
+
+Route::get('/discuss', function(){
+  return view('discuss');
+});
