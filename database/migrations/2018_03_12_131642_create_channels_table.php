5369ceaa208993979d5e523222c4028c8e2f5c7d
gabrieldornianu <gabrieldornianu@yahoo.com>
2018-03-12 16:13:06 +0200

    Created admin user seeder

--- database/migrations/2018_03_12_131642_create_channels_table.php
+++ database/migrations/2018_03_12_131642_create_channels_table.php
@@ -15,7 +15,7 @@ class CreateChannelsTable extends Migration
     {
         Schema::create('channels', function (Blueprint $table) {
             $table->increments('id');
-            $table->sting('title');
+            $table->string('title');
             $table->timestamps();
         });
     }
