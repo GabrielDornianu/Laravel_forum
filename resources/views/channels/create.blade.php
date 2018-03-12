d31c2f2362f54a538cd0365475659ed830ea2226
gabrieldornianu <gabrieldornianu@yahoo.com>
2018-03-12 17:51:39 +0200

    Cleaned the layout

--- resources/views/channels/create.blade.php
+++ resources/views/channels/create.blade.php
@@ -1,25 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Create a new channel</div>
+<div class="panel panel-default">
+    <div class="panel-heading">Create a new channel</div>
 
-                <div class="panel-body">
-                  <form action="{{ route('channels.store') }}" method="post">
-                    {{ csrf_field() }}
-                    <div class="form-group">
-                      <input type="text" name="title" value="" class="form-control">
-                    </div>
-                    <div class="form-group">
-                      <button type="submit" class="btn btn-success">Save channel</button>
-                    </div>
-                  </from>
-                </div>
-            </div>
+    <div class="panel-body">
+      <form action="{{ route('channels.store') }}" method="post">
+        {{ csrf_field() }}
+        <div class="form-group">
+          <input type="text" name="title" value="" class="form-control">
         </div>
+        <div class="form-group">
+          <button type="submit" class="btn btn-success">Save channel</button>
+        </div>
+      </from>
     </div>
 </div>
 @endsection
--- resources/views/channels/index.blade.php
+++ resources/views/channels/index.blade.php
@@ -1,48 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Channels</div>
+<div class="panel panel-default">
+    <div class="panel-heading">Channels</div>
 
-                <div class="panel-body">
-                    <table class="table table-hover">
-                      <thead>
-                        <th>
-                          Name
-                        </th>
-                        <th>
-                          Edit
-                        </th>
-                        <th>
-                          Delete
-                        </th>
-                      </thead>
-                      <tbody>
-                        @foreach($channels as $channel)
-                          <tr>
-                            <td>
-                              {{ $channel->title }}
-                            </td>
-                            <td>
-                              <a href="{{ route('channels.edit', ['channel' => $channel->id]) }}" class="btn btn-default">Edit</a>
-                            </td>
-                            <td>
-                              <form action="{{ route('channels.destroy', ['id' => $channel->id]) }}" method="post">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
-                                <button class="btn btn-danger" type="submit">X</button>
-                              </form>
-                            </td>
-                          </tr>
-                        @endforeach
-                      </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+    <div class="panel-body">
+        <table class="table table-hover">
+          <thead>
+            <th>
+              Name
+            </th>
+            <th>
+              Edit
+            </th>
+            <th>
+              Delete
+            </th>
+          </thead>
+          <tbody>
+            @foreach($channels as $channel)
+              <tr>
+                <td>
+                  {{ $channel->title }}
+                </td>
+                <td>
+                  <a href="{{ route('channels.edit', ['channel' => $channel->id]) }}" class="btn btn-default">Edit</a>
+                </td>
+                <td>
+                  <form action="{{ route('channels.destroy', ['id' => $channel->id]) }}" method="post">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+                    <button class="btn btn-danger" type="submit">X</button>
+                  </form>
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
     </div>
 </div>
 @endsection
