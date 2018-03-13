68e1762fa0587508f95dae8c942779c4e22a47b8
gabrieldornianu <gabrieldornianu@yahoo.com>
2018-03-13 16:59:59 +0200

    Added some structure to the forum discussions page

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -5,10 +5,20 @@
   @foreach($d as $discussion)
     <div class="panel panel-default">
       <div class="panel-heading">
-        <img src="{{ $discussion->user->avatar }}" width="70"/>
+        <img src="{{ $discussion->user->avatar }}" width="70"/>&nbsp;&nbsp;&nbsp;
+        <span>{{ $discussion->user->name }}</span>
+        <a href="{{ route('discussion', ['slug' => $discussion->slug]) }}" class="btn btn-default pull-right">View</a>
       </div>
       <div class="panel-body">
-        {{ $discussion->content }}
+        <h4 class="text-center">{{ $discussion->title }}</h4>
+        <p class="text-center">
+          {{ str_limit($discussion->content, 50) }}
+        </p>
+      </div>
+      <div class="panel-footer">
+        <p>
+          {{ $discussion->replies->count() }} replies
+        </p>
       </div>
     </div>
   @endforeach
